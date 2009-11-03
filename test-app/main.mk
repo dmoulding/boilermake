0984e3eb448e5b9b2befb9d92ed665b8f8e199e3
Dan Moulding <dmoulding@gmail.com>
2009-11-02 23:32:53 -0500

    Simplified the test-app sample main.mk.
    The old version was a bit verbose and didn't lend to clarity for people who
    might use it as a reference point for starting out with boilermake.

--- test-app/main.mk
+++ test-app/main.mk
@@ -1,20 +1,5 @@
-DEFAULT_CFG := debug
-
-ifeq "$(strip ${CFG})" ""
-    CFG := ${DEFAULT_CFG}
-endif
-
-INCDIRS := animals
-LDFLAGS  = -L${TARGET_DIR}
-
-ifeq "$(strip ${CFG})" "debug"
-    BUILD_DIR  := debug
-    CXXFLAGS   := -g -O0 -Wall -pipe
-    TARGET_DIR := debug
-else ifeq "$(strip ${CFG})" "release"
-    BUILD_DIR  := release
-    CXXFLAGS   := -g -O2 -Wall -pipe
-    TARGET_DIR := release
-endif
+CXXFLAGS := -g -O0 -Wall -pipe
+INCDIRS  := animals
+LDFLAGS  := -L.
 
 SUBMAKEFILES := talk.mk
