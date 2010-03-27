f507619883a398e1f14574e43c2be5fc0ab08a4b
Dan Moulding <dmoulding@gmail.com>
2010-03-27 10:27:48 -0400

    Explain the existence of the additional main.mk in the animals subdirectory

--- test-app/animals/main.mk
+++ test-app/animals/main.mk
@@ -1,3 +1,11 @@
+# The libanimals.a library can normally be built by itself (without also
+# building the "talk" program) by running "make libanimals.a" from within the
+# "test-app" directory.
+#
+# This main.mk exists solely for the purpose of also allowing users to build the
+# libanimals.a library by itself by running "make" from within the "animals"
+# subdirectory.
+
 BUILD_DIR  := ../build/animals
 TARGET_DIR := ..
 
